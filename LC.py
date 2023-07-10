390c9a5fa0304a2b9f05570804f560751e09ee7e
Dardo Altobelli <daltobelli@etrr.edu.ar>
2023-07-10 09:51:05 -0300

    bisbano circuit

---
+++ LC.py
@@ -0,0 +1,39 @@
+import schemdraw
+import schemdraw.elements as elm
+
+# LC circuit
+
+# Create a new schematic diagram object
+with schemdraw.Drawing() as d:
+    # Add the tag for the output pin
+    d += elm.Tag().label('Out')
+
+    # Add a wire to the left net
+    d += elm.Line().left()
+
+    # Save the position of the net
+    d.push()
+
+    # Add the capacitor
+    d += elm.Capacitor().down().label('C1\n10uF')
+
+    # Add the ground connection
+    d += elm.Ground()
+
+    # Return to the saved position of the net
+    d.pop()
+
+    # Add the Inductor
+    d += elm.Inductor().up().label('L1\n10Hy')
+
+    # Add a tag to indicate the voltage of this point
+    d += elm.Line().left().length(d.unit*.7).label('Vcc')
+
+    # Add the DC source (battery)
+    d += elm.BatteryCell().down().label('15v', loc='bottom')
+
+    d += elm.Line()
+    d += elm.Ground()
+
+    # Draw the schematic diagram
+    d.draw()
